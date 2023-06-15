62a37661b4fc490d1432a085672df79855a304b5
Anureet19 <anureet021999@gmail.com>
2023-06-14 20:00:14 -0400

    Added dependencies

--- app/src/main/java/com/example/horizona/Screens.kt
+++ app/src/main/java/com/example/horizona/Screens.kt
@@ -1,4 +1,24 @@
 package com.example.horizona
 
-class Screens {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+@Composable
+fun SessionScreen() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp, alignment = Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "YOU HAVE LOGGED IN")
+        Button(onClick = { }) {
+            Text("Log Out")
+        }
+    }
 }
