f9b443808d372afc01c17b1a714f19c60235b027
Anureet19 <anureet021999@gmail.com>
2023-06-15 15:07:22 -0400

    Added session screen

--- app/src/main/java/com/example/horizona/MainActivity.kt
+++ app/src/main/java/com/example/horizona/MainActivity.kt
@@ -22,25 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SessionScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HorizonATheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/horizona/Screens.kt
+++ app/src/main/java/com/example/horizona/Screens.kt
@@ -3,11 +3,16 @@ package com.example.horizona
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 @Composable
 fun SessionScreen() {
@@ -22,3 +27,4 @@ fun SessionScreen() {
         }
     }
 }
+
